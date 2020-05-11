094195be05ebcdeee58ce5927e1ef168f9ba5c56
DEBABRATA BAR <48706465+debabratabar@users.noreply.github.com>
2020-05-11 21:38:04 +0530

    Update LeadersOfTheArray.java

--- LeadersOfTheArray.java
+++ LeadersOfTheArray.java
@@ -1,5 +1,5 @@
 /*Problem Name :: Leaders of The Array
-Problrm Statement ::  An element is leader if it is greater than all the elements to its right side. 
+Problem Statement ::  An element is leader if it is greater than all the elements to its right side. 
 And the rightmost element is always a leader. For example int the array {16, 17, 4, 3, 5, 2}, leaders are 17, 5 and 2. 
 */
 
